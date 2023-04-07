a368a50f80d3b78a37f48f71ad9e2b2772d40508
lucas <lucas.monribot@ynov.com>
2023-04-07 17:09:19 +0200

    avance

--- TpFinale/puissance4.py
+++ TpFinale/puissance4.py
@@ -1,6 +1,6 @@
-# l'objectif est de faire un puissance 4 avec un IA qui joue contre l'utilisateur en allant cherche la meilleur solution possible
-# dans les parties précedentes en les stockant dans une base de donnée et en leurs associant un score afin de choisir la meilleur solution
-# pour le coup suivant en utilisant la bibliothèque pandas
+# L'objectif est de créer un jeu de Puissance 4 avec une IA qui joue contre l'utilisateur.
+# L'IA cherche la meilleure solution possible en se basant sur les parties précédentes stockées dans une base de données.
+# Chaque solution est associée à un score pour choisir le meilleur coup suivant en utilisant la bibliothèque pandas.
 
 import os
 import pickle
@@ -112,7 +112,6 @@ class Connect4GUI:
         self.connect4.__init__()
         self.draw_board()
 
-
     def click_event(self, event):
         if self.connect4.game_over:
             return
@@ -122,15 +121,31 @@ class Connect4GUI:
 
         if self.connect4.drop_piece(column):
             self.draw_board()
-            if self.connect4.is_winner(self.connect4.current_player):
+            if self.connect4.is_winner(PLAYER1):
                 self.connect4.game_over = True
-                self.connect4.winner = self.connect4.current_player
-                self.canvas.create_text(210, 210, text="Player {} wins!".format(self.connect4.winner), font=("Helvetica", 32))
+                self.connect4.winner = PLAYER1
+                self.canvas.create_text(210, 210, text="Player {} wins!".format(self.connect4.winner),
+                                        font=("Helvetica", 32))
+                self.connect4.append_and_save_game_data()
             elif self.connect4.is_board_full():
                 self.connect4.game_over = True
                 self.canvas.create_text(210, 210, text="Draw!", font=("Helvetica", 32))
+                self.connect4.append_and_save_game_data()
             else:
                 self.connect4.switch_player()
+                self.connect4.ai_move()
+                self.draw_board()
+                if self.connect4.is_winner(PLAYER2):
+                    self.connect4.game_over = True
+                    self.connect4.winner = PLAYER2
+                    self.canvas.create_text(210, 210, text="AI wins!", font=("Helvetica", 32))
+                    self.connect4.append_and_save_game_data()
+                elif self.connect4.is_board_full():
+                    self.connect4.game_over = True
+                    self.canvas.create_text(210, 210, text="Draw!", font=("Helvetica", 32))
+                    self.connect4.append_and_save_game_data()
+                else:
+                    self.connect4.switch_player()
 
 
 class Connect4AI(Connect4):
@@ -138,28 +153,25 @@ class Connect4AI(Connect4):
         super().__init__()
         self.db_file = 'game_data.csv'
         if os.path.exists(self.db_file):
-            with open(self.db_file, 'rb') as f:
-                self.game_data = pickle.load(f)
+            self.game_data = pd.read_csv(self.db_file)
         else:
             self.game_data = pd.DataFrame(columns=['moves', 'winner'])
 
     def save_game_data(self):
-        with open(self.db_file, 'wb') as f:
-            pickle.dump(self.game_data, f)
+        self.game_data.to_csv(self.db_file, index=False)
 
     def get_score(self, moves):
         score = 0
+        moves_str = '-'.join([f"{move[0]}{move[1]}{move[2]}" for move in moves])
         for _, game in self.game_data.iterrows():
             if game['winner'] == PLAYER2:
                 score_increment = 1
             else:
                 score_increment = -1
 
-            for move in moves:
-                if move in game['moves']:
-                    score += score_increment
-                else:
-                    break
+            if moves_str in game['moves']:
+                score += score_increment
+
         return score
 
     def choose_best_move(self):
@@ -169,7 +181,13 @@ class Connect4AI(Connect4):
 
         for move in possible_moves:
             moves_copy = self.moves.copy()
-            moves_copy.append((PLAYER2, move))
+            row = ROWS - 1
+            while row >= 0:
+                if self.board[row][move] == EMPTY:
+                    moves_copy.append((self.current_player, row, move))
+                    break
+                row -= 1
+
             score = self.get_score(moves_copy)
             if score > best_score:
                 best_score = score
@@ -187,75 +205,99 @@ class Connect4AI(Connect4):
                 self.game_over = True
                 self.winner = PLAYER2
                 print("AI wins!")
+                self.append_and_save_game_data()
             elif self.is_board_full():
                 self.game_over = True
                 print("Draw!")
+                self.append_and_save_game_data()
             else:
                 self.switch_player()
 
-    def get_possible_moves(self):
-        possible_moves = []
+    def get_valid_moves(self):
+        valid_moves = []
         for c in range(COLUMNS):
             if self.board[0][c] == EMPTY:
-                possible_moves.append(c)
-        return possible_moves
+                valid_moves.append(c)
+        return valid_moves
 
+    def append_and_save_game_data(self):
+        moves_str = '-'.join([f"{move[0]}{move[1]}{move[2]}" for move in self.moves])
+        new_row = pd.DataFrame({'moves': [moves_str], 'winner': [self.winner]})
+        self.game_data = pd.concat([self.game_data, new_row], ignore_index=True)
+        self.save_game_data()
 
-class Connect4GUIWithAI(Connect4GUI):
-    def __init__(self, connect4):
-        super().__init__(connect4)
+    def get_best_move_from_data(self, game_data):
+        valid_moves = self.get_valid_moves()
+        move_scores = {}
 
-    def click_event(self, event):
-        if self.connect4.game_over:
-            return
+        for move in valid_moves:
+            move_data = game_data[game_data['moves'].str.startswith(str(move))]
+            move_wins = move_data[move_data['winner'] == PLAYER1].shape[0]
+            move_losses = move_data[move_data['winner'] == PLAYER2].shape[0]
 
-        x = event.x
-        column = x // 60
-
-        if self.connect4.drop_piece(column):
-            self.draw_board()
-            if self.connect4.is_winner(PLAYER1):
-                self.connect4.game_over = True
-                self.connect4.winner = PLAYER1
-                self.canvas.create_text(210, 210, text="Player {} wins!".format(self.connect4.winner), font=("Helvetica", 32))
-                self.connect4.save_game_data()
-            elif self.connect4.is_board_full():
-                self.connect4.game_over = True
-                self.canvas.create_text(210, 210, text="Draw!", font=("Helvetica", 32))
-                self.connect4.save_game_data()
-
+            if move_wins + move_losses == 0:
+                move_scores[move] = 0.5
             else:
-                self.connect4.switch_player()
-                self.connect4.ai_move()
-                self.draw_board()
+                move_scores[move] = move_wins / (move_wins + move_losses)
 
+        best_move = max(move_scores, key=move_scores.get)
+        return best_move
+
+    def make_ai_move(self, game_data=None):
+        if game_data is None:
+            game_data = self.game_data
+
+        valid_moves = self.get_valid_moves()
+        move_scores = {}
+
+        for move in valid_moves:
+            move_data = self.get_move_data(move, game_data)
+            move_wins = move_data[move_data['winner'] == self.player1_id].shape[0]
+            move_losses = move_data[move_data['winner'] == self.player2_id].shape[0]
+            move_scores[move] = move_wins - move_losses
+
+        # Ajoutez cette vérification ici
+        if not move_scores:
+            self.game_over = True
+            return None
+
+        best_move = max(move_scores, key=move_scores.get)
+        return best_move
 
 
 class Connect4AIAutoPlay(Connect4AI):
-    def autoplay(self, num_games):
+    def autoplay(self, num_games=1, game_data=None):
         for _ in range(num_games):
-            while not self.game_over:
-                self.ai_move()
-                self.switch_player()
-            self.game_data = self.game_data._append({'moves': self.moves, 'winner': self.winner}, ignore_index=True)
             self.reset()
+            moves = []
+            while not self.game_over:
+                move = self.make_ai_move(game_data)
+                moves.append(move)
+                self.drop_piece(move)
+            moves_str = ' '.join(str(m) for m in moves)
+            new_row = pd.DataFrame({'moves': [moves_str], 'winner': [self.winner]})
+            self.game_data = pd.concat([self.game_data, new_row], ignore_index=True)
 
     def reset(self):
         self.__init__()
+        self.game_over = False
+        self.winner = None
+        self.moves = []
+
+    def get_move_data(self, move, game_data):
+        move_data = game_data[game_data['move'] == move]
+        return move_data
 
 
 
+
+game_data = pd.read_csv('game_data.csv')
 
 
 if __name__ == '__main__':
-    # Entraînement de l'IA en jouant contre elle-même
+    game_data = pd.read_csv('game_data.csv')
     connect4_train = Connect4AIAutoPlay()
-    connect4_train.autoplay(num_games=10)
+    connect4_train.autoplay(num_games=10, game_data=game_data)
     connect4_train.save_game_data()
-
-
-# if __name__ == '__main__':
-#     connect4 = Connect4AI()
-#     Connect4GUIWithAI(connect4)
 
 
